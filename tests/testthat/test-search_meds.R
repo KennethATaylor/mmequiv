417f36aedd819b53c3c42f6c1c6d520f3f4b93bf
Ken Taylor <kenneth.taylor.dpt@gmail.com>
2025-04-09 08:15:43 -0400

    Update tests for get_med_list() and add tests for search_meds()

---
+++ tests/testthat/test-search_meds.R
@@ -0,0 +1,13 @@
+with_mock_dir("search_meds", {
+  test_that("Returns data.frame", {
+    oxy_list <- search_meds("oxy")
+    expect_true(is.data.frame(oxy_list))
+  })
+  
+  test_that("Able to retrieve all 6 medications with 'mcg' in the name", {
+    mcg <- search_meds("mcg")
+    expect_length(mcg$med_name, 6)
+    expect_length(mcg$cf, 6)
+  })
+  
+})
